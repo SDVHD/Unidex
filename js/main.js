a4e013d004a7d3ecc7ed5b57ca315a228a012cbd
Simon Lissa <6142822-simon.lissa@users.noreply.gitlab.com>
2020-07-01 20:19:59 +0200

    Big Update

---
+++ js/main.js
@@ -0,0 +1,17 @@
+function changebuttonstyle() {
+    if (document.getElementsByClassName("nav_button").value = "home") {
+        document.getElementById("btn_nav_home").style.color = 'lightskyblue';
+    }
+    else if (document.getElementsByClassName("nav_button").value = "features") {
+        document.getElementById("btn_nav_features").style.color = 'lightskyblue';
+    }
+    else if (document.getElementsByClassName("nav_button").value = "price") {
+        document.getElementById("btn_nav_price").style.color = 'lightskyblue';
+    }
+    else if (document.getElementsByClassName("nav_button").value = "docu") {
+        document.getElementById("btn_nav_Docu").style.color = 'lightskyblue';
+    }
+    else if (document.getElementsByClassName("nav_button").value = "contact") {
+        document.getElementById("btn_nav_contact").style.color = 'lightskyblue';
+    }
+}

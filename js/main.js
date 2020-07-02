2529f512d9b21025714e81a58dd8ecdb80682d63
Simon Lissa <6142822-simon.lissa@users.noreply.gitlab.com>
2020-07-02 21:55:30 +0200

    JS Update

--- js/main.js
+++ js/main.js
@@ -2,27 +2,22 @@ function openSite(btn_num) {
     switch(btn_num) {
         case 1: {
             window.open("index.html");
-            document.getElementById("btn_nav_home").style.color = 'lightskyblue';
             changebuttonstyle(1);
         }
         case 2: {
             window.open("features.html");
-            document.getElementById("btn_nav_home").style.color = 'lightskyblue';
             changebuttonstyle(2);
         }
         case 3: {
             window.open("price.html");
-            document.getElementById("btn_nav_home").style.color = 'lightskyblue';
             changebuttonstyle(3);
         }
         case 4: {
             window.open("docu.html");
-            document.getElementById("btn_nav_home").style.color = 'lightskyblue';
             changebuttonstyle(4);
         }
         case 5: {
-            window.open("contact.html");
-            document.getElementById("btn_nav_home").style.color = 'lightskyblue';
+            .open("contact.html");
             changebuttonstyle(5);
         }
     }
@@ -43,7 +38,7 @@ function changebuttonstyle(btn_style) {
             document.getElementById("btn_nav_Docu").style.color = 'lightskyblue';
         }
         case 5: {
-            document.getElementById("btn_nav_contact").style.color = 'lightskyblue';;
+            document.getElementById("btn_nav_contact").style.color = 'lightskyblue';
         }
     }
 }
